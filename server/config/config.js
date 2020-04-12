130f5e5c6b467ba2a2f84f40232e21b16bc8cb2a
YukJiSoo <wltn3231@naver.com>
2020-04-12 22:17:13 +0900

    [feat(docker)] DB 실행환경 docker-compose 파일로 구성
    docker를 사용하여 local과 분리된 개발환경 구성을 가능하게했습니다.
    adminer container도 같이 생성하여 web admin tool을 이용할 수 있습니다.

--- server/config/config.js
+++ server/config/config.js
@@ -2,6 +2,7 @@ import path from 'path';
 import { config } from 'dotenv';
 
 config({ path: path.join(__dirname, `../../.env.${process.env.NODE_ENV}`) });
+config({ path: path.join(__dirname, `../../db/.env.${process.env.NODE_ENV}`) });
 
 const {
   DB_USER,
