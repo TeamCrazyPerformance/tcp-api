f89f4a58692a820f7d4f90dd5556535603716f3a
lallaheeee <ooop0422@gmail.com>
2020-05-20 18:29:44 +0900

    [chore] 시드 데이트 추가

--- server/seeders/mock/user.js
+++ server/seeders/mock/user.js
@@ -1,13 +1,14 @@
 export default [
   {
     grade: 13,
-    school_register: '재학',
+    school_register: '졸업',
     phone: '010-1234-5678',
     birth: '1000/00/00',
     email: 'test@test.com',
     username: '김테스트',
     blog: 'test@test.com',
     avatar: 'https://avatars2.githubusercontent.com/u/22452742?v=4',
+    membership_id: 1,
     github_id: 'test',
     github_username: 'test',
     created_at: new Date(),
